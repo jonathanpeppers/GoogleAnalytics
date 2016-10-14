48e49ed2911d18304a2d1598f3c05c8530eb6a45
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-10-13 22:26:29 -0500

    Bash script to run objective sharpie + default output

--- GoogleAnalytics.iOS/Structs.cs
+++ GoogleAnalytics.iOS/Structs.cs
@@ -1,5 +1,30 @@
-﻿using System;
+using System;
+using ObjCRuntime;
 
 namespace GoogleAnalytics.iOS
 {
+	[Native]
+	public enum GAILogLevel : nuint
+	{
+		None = 0,
+		Error = 1,
+		Warning = 2,
+		Info = 3,
+		Verbose = 4
+	}
+
+	[Native]
+	public enum GAIDispatchResult : nuint
+	{
+		NoData,
+		Good,
+		Error
+	}
+
+	public enum GAIErrorCode : uint
+	{
+		NoError = 0,
+		DatabaseError,
+		NetworkError
+	}
 }
